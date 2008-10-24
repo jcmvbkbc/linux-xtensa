729f213f8292a4285411a964053cd33b8997f15f
Joe Taylor <joe@tensilica.com>
2008-10-23 20:05:23 -0700

    [XTENSA] Provide SMP-safe bitops functions
    Masking interrupts is not atomic in SMP systems.  Use the
    S32C1I instruction instead because that works across cores.
    
    Signed-off-by: Joe Taylor <joe@tensilica.com>

--- include/asm-xtensa/bitops.h
+++ include/asm-xtensa/bitops.h
@@ -92,96 +92,125 @@ static inline int fls (unsigned int x)
  *
  * First, the atomic bitops. These use native endian.
  */
+
 static inline void set_bit(unsigned int bit, volatile unsigned long *p)
 {
-	unsigned long flags;
-	unsigned long mask = 1UL << (bit & 31);
+        unsigned long tmp, value;
+        unsigned long mask = 1UL << (bit & 31);
 
-	p += bit >> 5;
+        p += bit >> 5;
 
-	local_irq_save(flags);
-	*p |= mask;
-	local_irq_restore(flags);
+        __asm__ __volatile__(
+"1:     l32i    %1, %3, 0       \n"
+"       wsr     %1, SCOMPARE1   \n"
+"       or      %0, %1, %2      \n"
+"       s32c1i  %0, %3, 0       \n"
+"       bne     %0, %1, 1b      \n"
+        : "=&a" (tmp), "=&a" (value)
+        : "a" (mask), "a" (p)
+        : "memory" );
 }
 
 static inline void clear_bit(unsigned int bit, volatile unsigned long *p)
 {
-	unsigned long flags;
-	unsigned long mask = 1UL << (bit & 31);
+        unsigned long tmp, value;
+        unsigned long mask = 1UL << (bit & 31);
 
-	p += bit >> 5;
+        p += bit >> 5;
 
-	local_irq_save(flags);
-	*p &= ~mask;
-	local_irq_restore(flags);
+        __asm__ __volatile__(
+"1:     l32i    %1, %3, 0       \n"
+"       wsr     %1, SCOMPARE1   \n"
+"       and     %0, %1, %2      \n"
+"       s32c1i  %0, %3, 0       \n"
+"       bne     %0, %1, 1b      \n"
+        : "=&a" (tmp), "=&a" (value)
+        : "a" (~mask), "a" (p)
+        : "memory" );
 }
 
 static inline void change_bit(unsigned int bit, volatile unsigned long *p)
 {
-	unsigned long flags;
-	unsigned long mask = 1UL << (bit & 31);
+        unsigned long tmp, value;
+        unsigned long mask = 1UL << (bit & 31);
 
-	p += bit >> 5;
+        p += bit >> 5;
 
-	local_irq_save(flags);
-	*p ^= mask;
-	local_irq_restore(flags);
+        __asm__ __volatile__(
+"1:     l32i    %1, %3, 0       \n"
+"       wsr     %1, SCOMPARE1   \n"
+"       xor     %0, %1, %2      \n"
+"       s32c1i  %0, %3, 0       \n"
+"       bne     %0, %1, 1b      \n"
+        : "=&a" (tmp), "=&a" (value)
+        : "a" (mask), "a" (p)
+        : "memory" );
 }
 
 static inline int
 test_and_set_bit(unsigned int bit, volatile unsigned long *p)
 {
-	unsigned long flags;
-	unsigned int res;
-	unsigned long mask = 1UL << (bit & 31);
+        unsigned long tmp, value;
+        unsigned long mask = 1UL << (bit & 31);
 
-	p += bit >> 5;
+        p += bit >> 5;
 
-	local_irq_save(flags);
-	res = *p;
-	*p = res | mask;
-	local_irq_restore(flags);
+        __asm__ __volatile__(
+"1:     l32i    %1, %3, 0       \n"
+"       wsr     %1, SCOMPARE1   \n"
+"       or      %0, %1, %2      \n"
+"       s32c1i  %0, %3, 0       \n"
+"       bne     %0, %1, 1b      \n"
+        : "=&a" (tmp), "=&a" (value)
+        : "a" (mask), "a" (p)
+        : "memory" );
 
-	return res & mask;
+        return tmp & mask;
 }
 
 static inline int
 test_and_clear_bit(unsigned int bit, volatile unsigned long *p)
 {
-	unsigned long flags;
-	unsigned int res;
-	unsigned long mask = 1UL << (bit & 31);
+        unsigned long tmp, value;
+        unsigned long mask = 1UL << (bit & 31);
 
-	p += bit >> 5;
+        p += bit >> 5;
 
-	local_irq_save(flags);
-	res = *p;
-	*p = res & ~mask;
-	local_irq_restore(flags);
+        __asm__ __volatile__(
+"1:     l32i    %1, %3, 0       \n"
+"       wsr     %1, SCOMPARE1   \n"
+"       and     %0, %1, %2      \n"
+"       s32c1i  %0, %3, 0       \n"
+"       bne     %0, %1, 1b      \n"
+        : "=&a" (tmp), "=&a" (value)
+        : "a" (~mask), "a" (p)
+        : "memory" );
 
-	return res & mask;
+        return tmp & mask;
 }
 
 static inline int
 test_and_change_bit(unsigned int bit, volatile unsigned long *p)
 {
-	unsigned long flags;
-	unsigned int res;
-	unsigned long mask = 1UL << (bit & 31);
+        unsigned long tmp, value;
+        unsigned long mask = 1UL << (bit & 31);
 
-	p += bit >> 5;
+        p += bit >> 5;
 
-	local_irq_save(flags);
-	res = *p;
-	*p = res ^ mask;
-	local_irq_restore(flags);
+        __asm__ __volatile__(
+"1:     l32i    %1, %3, 0       \n"
+"       wsr     %1, SCOMPARE1   \n"
+"       xor     %0, %1, %2      \n"
+"       s32c1i  %0, %3, 0       \n"
+"       bne     %0, %1, 1b      \n"
+        : "=&a" (tmp), "=&a" (value)
+        : "a" (mask), "a" (p)
+        : "memory" );
 
-	return res & mask;
+        return tmp & mask;
 }
 
 #include <asm-generic/bitops/non-atomic.h>
-
-
 #include <asm-generic/bitops/find.h>
 #include <asm-generic/bitops/ext2-non-atomic.h>
 
