16123320084b647a59d8697ec53db7dbd695c4f6
Pete Delaney <piet@tensilica.com>
2010-12-01 22:24:19 -0800

    [XTENSA] Updated .xt-gdbinit and comment on reason for 128M Default Mem Size.
    LX200 has run LTP and Audio Test for 6 days 20 hours; merging to master.
    
    Signed-off-by: Pete Delaney <piet@tensilica.com>

--- arch/xtensa/platforms/iss/include/platform/hardware.h
+++ arch/xtensa/platforms/iss/include/platform/hardware.h
@@ -16,11 +16,13 @@
 #define _XTENSA_PLATFORM_ISS_HARDWARE_H
 
 /*
- * Memory configuration.
+ * Memory configuration:
+ *   Memory Limited by Kernel being mapped 
+ * 	Cached: 0XD000,0000 to 0XD7FF,FFFF
+ * 	Bypass: 0XD800,0000 to 0XDFFF,FFFF
  */
-
 #define PLATFORM_DEFAULT_MEM_START	0x00000000
-#define PLATFORM_DEFAULT_MEM_SIZE	0x08000000
+#define PLATFORM_DEFAULT_MEM_SIZE	0x08000000	/* 13,4217,728 128M */
 
 /*
  * Interrupt configuration.
