5fcf3ea6ec5603179c36571bb05e2f56c4da77e3
Max Filippov <jcmvbkbc@gmail.com>
2013-04-08 01:29:19 +0400

    xtensa: WIP: provide custom CALLER_ADDR* implementations
    Definition of CALLER_ADDR* through __builtin_return_address makes
    compiler insert calls to __xtensa_libgcc_window_spill, which in turn
    makes syscall fast_syscall_spill_registers that clobbers registers.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/include/asm/ftrace.h
+++ arch/xtensa/include/asm/ftrace.h
@@ -1,1 +1,22 @@
-/* empty */
+#ifndef _XTENSA_FTRACE_H
+#define _XTENSA_FTRACE_H
+
+#include <asm/processor.h>
+
+#define HAVE_ARCH_CALLER_ADDR
+#ifdef CONFIG_FRAME_POINTER
+extern unsigned long return_address(unsigned level);
+#define CALLER_ADDR0 return_address(0)
+#define CALLER_ADDR1 return_address(1)
+#define CALLER_ADDR2 return_address(2)
+#define CALLER_ADDR3 return_address(3)
+#else
+#define CALLER_ADDR0 ({ unsigned long a0, a1; \
+	__asm__ __volatile__ ("mov %0, a0 ; mov %1, a1\n" : "=r"(a0), "=r"(a1) : : ); \
+	 MAKE_PC_FROM_RA(a0, a1); })
+#define CALLER_ADDR1 (0)
+#define CALLER_ADDR2 (0)
+#define CALLER_ADDR3 (0)
+#endif
+
+#endif /* _XTENSA_FTRACE_H */
