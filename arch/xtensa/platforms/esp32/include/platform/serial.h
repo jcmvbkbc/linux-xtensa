e93b167b5bcb153154f58a584e8c942e8a8f7682
Max Filippov <jcmvbkbc@gmail.com>
2019-12-15 11:25:58 -0800

    xtensa: esp32: add platform
    Add platform directory, defconfig, device tree and entry in platforms
    selector in Kconfig.
    
    Generic dts part contains devices with fixed address properties,
    config-specific contains stuff that moves depending on other
    configuration parameters, like IDF version used for the bootloader.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ arch/xtensa/platforms/esp32/include/platform/serial.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef __ASM_XTENSA_ESP32_SERIAL_H
+#define __ASM_XTENSA_ESP32_SERIAL_H
+
+#define BASE_BAUD 115200
+
+#endif /* __ASM_XTENSA_ESP32_SERIAL_H */
