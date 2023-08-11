8c1fb47acdb23c156acdf7b6f286db404d2c508f
Max Filippov <jcmvbkbc@gmail.com>
2023-08-11 06:32:35 -0700

    misc/esp32-ipc: introduce generic IPC for ESP32
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ include/linux/esp32-ipc-api.h
@@ -0,0 +1,11 @@
+#ifndef ESP32_IPC_API_H
+#define ESP32_IPC_API_H
+
+#include <linux/types.h>
+
+int esp32_ipc_register_rx(void *ipc, u32 addr, void *p,
+			  int (*rx)(void *p, void *data),
+			  void (*rx_batch_done)(void *p, u32 tag));
+int esp32_ipc_tx(void *ipc, u32 addr, void *info, u32 *tag);
+
+#endif
