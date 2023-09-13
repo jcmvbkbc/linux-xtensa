6dab6b5c4a918db7df69f95617040f0cd7a59157
Max Filippov <jcmvbkbc@gmail.com>
2023-09-12 17:37:58 -0700

    WIP: xtensa: esp32: low level debug code
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/platforms/esp32/setup.c
+++ arch/xtensa/platforms/esp32/setup.c
@@ -12,6 +12,53 @@
 #include <asm/processor.h>
 #include <asm/timex.h>
 
+#ifdef DEBUG
+#if 1
+void dbg_echo(const char *s)
+{
+	//volatile void __iomem *base = (volatile void __iomem *)0x3ff40000;
+	volatile void __iomem *base = (volatile void __iomem *)0x60000000;
+
+	while ((readl(base + UART_STATUS_REG) & UART_TXFIFO_CNT_MASK) != 0)
+		;
+
+	while (*s) {
+		if (*s == '\n')
+			writel('\r', base + UART_FIFO_REG);
+		writel(*s, base + UART_FIFO_REG);
+		++s;
+	}
+}
+#else
+void dbg_echo(const char *s)
+{
+	volatile void __iomem *base = (volatile void __iomem *)0x60038000;
+
+	while (!(readl(base + USB_SERIAL_JTAG_EP1_CONF_REG) &
+		 USB_SERIAL_JTAG_SERIAL_IN_EP_DATA_FREE_MASK))
+		;
+
+	while (*s) {
+		if (*s == '\n')
+			writel('\r', base + USB_SERIAL_JTAG_EP1_REG);
+		writel(*s, base + USB_SERIAL_JTAG_EP1_REG);
+		++s;
+	}
+}
+#endif
+
+void dbg_printf(const char *fmt, ...)
+{
+	va_list ap;
+	char buf[256];
+
+	va_start(ap, fmt);
+	vsnprintf(buf, sizeof(buf), fmt, ap);
+	va_end(ap);
+	dbg_echo(buf);
+}
+#endif
+
 void __init platform_setup(char **p)
 {
 	volatile void __iomem *base;
